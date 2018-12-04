a8986d2646c07ee567d34749b2d8e713cf40f48b
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-04 15:17:22 -0800

    save function now works - all tests dealing with GetAll are uncommented

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -66,11 +66,46 @@ namespace ToDoList.Models
       }
     }
 
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this._description;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery(); 
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if(!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+        return (descriptionEquality);
+      }
+    }
+
     public static Item Find(int searchId)
     {
       // Temporarily returning dummy item to get beyond compiler errors, until we refactor to work with database.
       Item dummyItem = new Item("dummy item");
       return dummyItem;
     }
+
+
   }
 }

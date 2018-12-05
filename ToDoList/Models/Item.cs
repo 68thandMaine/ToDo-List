261b4b02f6782e866c00ec4cc734ab21dd16ba0d
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-04 16:18:48 -0800

    Add find ID functionality

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -41,7 +41,7 @@ namespace ToDoList.Models
       {
         int itemId = rdr.GetInt32(0);
         string itemDescription = rdr.GetString(1);
-        Item newItem = new Item(itemDescription);
+        Item newItem = new Item(itemDescription, itemId);
         allItems.Add(newItem);
       }
       conn.Close();
@@ -101,11 +101,47 @@ namespace ToDoList.Models
       }
     }
 
-    public static Item Find(int searchId)
+    public static Item Find(int id)
     {
+      //Open a connectoin to the database
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      //Pass a command to SQL
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      //Remember to use @thisId for the placeholder
+      cmd.CommandText = @"Select * FROM `items` WHERE id = @thisId;";
+
+      //Create the MySqlParamater object for feeding VALUES
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+
+      //Associate the parameter object with the SqlCommand. Because the find method returns something we need to use ExecuteReader() and cast the data as a MySqlDataReader.
+      cmd.Parameters.Add(thisId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+      //Initiate reading the database with a while loop
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+
+      //Create and return a new Item object with the values that were located
+      Item foundItem = new Item(itemDescription, itemId);
+
+      //Close a connection to the database
+      conn.Close();
+      if(conn!=null)
+      {
+        conn.Dispose();
+      }
+
       // Temporarily returning dummy item to get beyond compiler errors, until we refactor to work with database.
-      Item dummyItem = new Item("dummy item");
-      return dummyItem;
+      // Item dummyItem = new Item("dummy item");
+      // return dummyItem;
     }
 
 

4414f8e3803dcb55677e2bab3ef48bf9eb678105
Student <example@example.com>
2018-12-06 09:56:02 -0800

    remove create test methods from homecontroller

--- ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
+++ ToDoList.Tests/ControllerTests/ItemsControllerTests.cs
@@ -10,18 +10,7 @@ namespace ToDoList.Tests
     [TestClass]
     public class ItemsControllerTest
     {
-      [TestMethod]
-      public void Create_ReturnsCorrectActionType_RedirectToActionResult()
-      {
-        //Arrange
-        ItemsController controller = new ItemsController();
-
-        //Act
-        ActionResult view = controller.Create("Walk the dog");
-
-        //Assert
-        Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
-      }
+  
 
     }
 }
